7db9716f32dfa2b0183c4a919f256f4efec3dd5e
Robert Colin <cfflymolo@gmail.com>
2013-05-04 15:32:09 -0700

    - Added in AFNetworking third party framework to the project - Added some basic data validation to the log in page - Added a Core Data Model file to the project

--- Office Hours/Controllers/LoginViewController.m
+++ Office Hours/Controllers/LoginViewController.m
@@ -8,26 +8,53 @@
 
 #import "LoginViewController.h"
 
-@interface LoginViewController ()
+#define H_USERNAME_TEXT_FIELD_TAG 1
+#define H_PASSWORD_TEXT_FIELD_TAG 2
+
+@interface LoginViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @end
 
 @implementation LoginViewController
 
+- (void)viewDidLoad
+{
+    [self.navigationController setNavigationBarHidden:YES
+                                             animated:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.usernameTextField becomeFirstResponder];
+}
+
 - (IBAction)signInPressed
 {
-    NSLog(@"Sign In Pressed");
-    
     if ([self validateLoginCredentials]) {
-
+        // Shoot off HTML request
     }
 }
+
+#define EMPTY_STRING @""
 
 - (BOOL)validateLoginCredentials
 {
     NSString *username = self.usernameTextField.text;
     NSString *passowrd = self.passwordTextField.text;
+    
+    if ([username isEqualToString:EMPTY_STRING] ||
+        [passowrd isEqualToString:EMPTY_STRING]) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                            message:@"Invalid username/password"
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil, nil];
+        [alertView show];
+        [self.usernameTextField becomeFirstResponder];
+    }
     
     NSLog(@"username: %@, password: %@", username, passowrd);
     
@@ -40,6 +67,23 @@
 {
     self.usernameTextField.text =
     self.passwordTextField.text = @"";
+}
+
+#pragma mark - UITextFieldDelegate Methods
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    switch (textField.tag) {
+        case H_USERNAME_TEXT_FIELD_TAG:
+            [self.passwordTextField becomeFirstResponder];
+            break;
+        case H_PASSWORD_TEXT_FIELD_TAG:
+            [self.passwordTextField resignFirstResponder];
+            [self signInPressed];
+            break;
+    }
+    
+    return YES;
 }
 
 @end
