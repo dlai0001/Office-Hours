9f4a6feb08ff5cd42fa659a78b037134515ef3d7
Robert Colin <cfflymolo@gmail.com>
2013-05-04 12:19:51 -0700

    Initial Commit

---
+++ Office Hours/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Office Hours
+//
+//  Created by Robert Colin on 5/4/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Office Hours/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Office Hours
+//
+//  Created by Robert Colin on 5/4/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
